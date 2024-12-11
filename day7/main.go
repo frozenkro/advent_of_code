e996af7212a0656f63063ba7f972ed83baed5979
kaup <kalebroyolson+github@gmail.com>
2024-12-10 23:59:56 -0600

    day 7 wip

--- day7/main.go
+++ day7/main.go
@@ -1,6 +1,12 @@
 package main
 
-import ()
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
 
 type equation struct {
 	total    int
@@ -11,4 +17,53 @@ func main() {
 
 }
 
-func getData(path string)
+func getData(path string) ([]equation, error) {
+	f, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+
+	results := []equation{}
+	sn := bufio.NewScanner(f)
+	for sn.Scan() {
+		nums := strings.Split(sn.Text(), " ")
+		if len(nums) < 2 {
+			return nil, fmt.Errorf("Invalid row found: '%v'", sn.Text())
+		}
+
+		totalStr := strings.Trim(nums[0], ":")
+		total, err := strconv.Atoi(totalStr)
+		if err != nil {
+			return nil, fmt.Errorf("Error converting total to int for row '%v', value: '%v'", sn.Text(), totalStr)
+		}
+
+		o := []int{}
+		for _, v := range nums[1:] {
+			n, err := strconv.Atoi(v)
+			if err != nil {
+				return nil, fmt.Errorf("Error converting operand to int for row '%v', value: '%v'", sn.Text(), n)
+			}
+			o = append(o, n)
+		}
+
+		equation := equation{total: total, operands: o}
+		results = append(results, equation)
+	}
+
+	return results, nil
+}
+
+func validCount(equations []equation) int {
+	total := 0
+	for _, e := range equations {
+		if e.isValid() {
+			total++
+		}
+	}
+	return total
+}
+
+func (e equation) isValid() bool {
+	//todo
+	return false
+}
