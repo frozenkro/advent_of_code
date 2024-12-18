0d82738b8d8c93eab331d05b80eec585062c51b6
Kaleb Olson <kalebroyolson+github@gmail.com>
2024-12-17 19:11:45 -0600

    day9 p1 complete

--- day9/main.go
+++ day9/main.go
@@ -1,33 +1,62 @@
 package main
 
-import "strconv"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+const free = -1
+
+type file struct {
+	address int
+	size    int
+}
 
 func main() {
+	d, err := getData("./input")
+	if err != nil {
+		fmt.Printf("Error reading input: %v\n", err)
+	}
 
+	result := DefragChksum(d)
+	fmt.Printf("Result: %d\n", result)
+}
+
+func getData(path string) ([]byte, error) {
+	f, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+	sc := bufio.NewScanner(f)
+
+	sc.Scan()
+	return []byte(sc.Text()), nil
 }
 
 func DefragChksum(data []byte) int {
 	dataMap := createDataMap(data)
-	_ = dataMap
-	return 0
+	result := defragDataMap(dataMap)
+	return result
 }
 
-func createDataMap(src []byte) []byte {
-	dataMap := []byte{}
+func createDataMap(src []byte) []int {
+	dataMap := []int{}
 	isEven := true
 	for i, v := range src {
-		var ch byte
+		var value int
 		if isEven {
-			ch = '.'
+			value = i / 2
 		} else {
-			ch = byte(i / 2)
+			value = free
 		}
 
 		intV, err := strconv.Atoi(string(v))
 		if err != nil {
 			panic(err)
 		}
-		frag := createFrag(ch, intV)
+		frag := createFrag(value, intV)
 
 		dataMap = append(dataMap, frag...)
 		isEven = !isEven
@@ -35,10 +64,33 @@ func createDataMap(src []byte) []byte {
 	return dataMap
 }
 
-func createFrag(ch byte, ln int) []byte {
-	f := make([]byte, ln)
+func createFrag(value int, ln int) []int {
+	f := make([]int, ln)
 	for i := 0; i < ln; i++ {
-		f[i] = ch
+		f[i] = value
 	}
 	return f
+}
+
+func defragDataMap(d []int) int {
+	i := 0
+	ii := len(d) - 1
+
+	total := 0
+
+	for i < ii {
+
+		for d[i] != free {
+			total += i * int(d[i])
+			i++
+		}
+		for d[ii] == free {
+			ii--
+		}
+
+		d[i] = d[ii]
+		d[ii] = free
+
+	}
+	return total
 }
